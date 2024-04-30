9e9fd3a7d178a52a776f00735642228c1b65d011
Supanee Patsa <156906258+Supanee-patsa@users.noreply.github.com>
2024-04-30 15:13:15 +0200

    add regiter form

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,8 +1,30 @@
 import "../index.css";
 const Register = () => {
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState ("");
+    const [password, setPassword] = useState ("");
+
+    const checkEmail = (users) => {
+        const user = users.find((user) => user.email === email);
+        if (user) return user;
+    };
+
+    const handleSubmit = async () => {
+      const user = await axios
+        .get("/users")
+        .then((res) => checkEmail(res.data, email));
+
+      if (user) {
+        alert ("User already exists!");
+      } else {
+        const user = {username, email, password };
+        axios.post("/users", user).then(alert("User created!"));
+      }
+    };
+
     return (
         <div className="container">
-             <form>
+             <form className="form-container">
                <h1>Register</h1>
                <label>
                    <input type="text"
