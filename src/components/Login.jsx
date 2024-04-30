769841caefbae4fd38681966dabc8a0a47fe82a4
Supanee Patsa <156906258+Supanee-patsa@users.noreply.github.com>
2024-04-30 16:44:56 +0200

    add login form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,23 +1,64 @@
 import "../index.css";
+import {useState} from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 const Login = () => {
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+
+    const navigate = useNavigate();
+
+    const checkUser = (users) => {
+      const user = users.find(
+        (user) => user.Name === Name && user.password === password);
+        console.log(user)
+      if (user.name === name && user.password === password) return user;
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault ();
+
+        if (name ==="" || password === "") {
+            alert ("All fields are required!");
+        }
+
+        const user = await axios
+        .get("/users")
+        .then((res) => checkUser(res.data, name, password))
+        .catch ((error) => {
+            console.log(error)
+        })
+
+        if (user.name === name && user.password === password) {
+            navigate("/");
+        }
+    }
+
     return (
         <div className="container">
-             <form>
+             <form className="form-container">
                <h1>Login</h1>
                 <label>
                     <input type="Text"
-                         placeholder="Name"  />
+                     placeholder="Name" 
+                     value={name}
+                     onChange={(e) => setName(e.target.value)} 
+                    />
                          
                 </label>
                 <label>
                     <input type="password"
-                         placeholder="Password"   />   
+                     placeholder="Password"
+                     value={password}
+                     onChange={(e) => setPassword(e.target.value)}  
+                    />   
                              
                 </label>
                 <button className="btn" type="submit"
                  >
                 
-                    <p>Submit</p>     
+                    <p>Log in</p>     
                 </button>
               </form>
     </div>  
